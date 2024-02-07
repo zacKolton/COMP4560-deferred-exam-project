aa5aaabe40a6d332b1acd1df303c2591619d864a
zacKolton <zac.kolton@hotmail.com>
2024-02-06 23:55:13 -0600

    updating app code

--- app_logic/app.py
+++ app_logic/app.py
@@ -1,15 +1,22 @@
-
-import tkinter as tk
-from tkinter import filedialog, messagebox
-import shutil
 import os
+import shutil
+from tkinter import filedialog, messagebox
 
 
 class App:
     def __init__(self, ui):
         self.ui = ui
 
-    def upload_and_save(self):
+    def upload_file(self):
+        file_path = filedialog.askopenfilename(filetypes=[("Excel files", "*.xlsx *.xls")])
+        result = self.process_file(file_path)
+        if result:
+            messagebox.showinfo("Success", f"File has been saved as '{os.path.basename(result)}' in Downloads.")
 
-    
-    def 
+    def process_file(self, file_path, new_file_name="data.xlsx"):
+        if file_path:
+            downloads_path = os.path.join(os.path.expanduser('~'), 'Downloads') 
+            new_file_path = os.path.join(downloads_path, new_file_name)
+            #shutil.copy(file_path, new_file_path)
+            return new_file_path
+        return None
