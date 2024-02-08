024214bee61fa1e0cdf26210b006c421bfdb17d4
zacKolton <zac.kolton@hotmail.com>
2024-02-07 21:32:51 -0600

    added requirements.txt and added scheduler structure

--- app_logic/app.py
+++ app_logic/app.py
@@ -1,6 +1,5 @@
-import os
-import shutil
 from tkinter import filedialog, messagebox
+from scheduler.schedule_creator import ScheduleCreator
 
 
 class App:
@@ -9,14 +8,11 @@ class App:
 
     def upload_file(self):
         file_path = filedialog.askopenfilename(filetypes=[("Excel files", "*.xlsx *.xls")])
-        result = self.process_file(file_path)
-        if result:
-            messagebox.showinfo("Success", f"File has been saved as '{os.path.basename(result)}' in Downloads.")
-
-    def process_file(self, file_path, new_file_name="data.xlsx"):
         if file_path:
-            downloads_path = os.path.join(os.path.expanduser('~'), 'Downloads') 
-            new_file_path = os.path.join(downloads_path, new_file_name)
-            #shutil.copy(file_path, new_file_path)
-            return new_file_path
-        return None
+            try:
+                scheduler = ScheduleCreator()
+                schedule = scheduler.create_schedule(file_path)
+
+                messagebox.showinfo("Success", "The schedule has been successfully created.")
+            except Exception as e:
+                 messagebox.showerror("Error", f"An error occurred: {e}")
